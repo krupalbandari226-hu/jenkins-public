0b871ace691fa9264945d2415c9dceca75571f3b
krupalbandari226-hu <krupalbandari226@gmail.com>
2026-02-18 15:33:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("This is DEV Branch Application");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.write("DEV Branch NodeJS Application Running");
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
