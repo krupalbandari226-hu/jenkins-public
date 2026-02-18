d40f2e35a91ae28c7df0b7a68af6718e88f01b84
krupalbandari226-hu <krupalbandari226@gmail.com>
2026-02-18 15:34:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("This is QA Branch Application")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "QA Branch Flask Application Running"
+
+app.run(host='0.0.0.0', port=5000)
+
